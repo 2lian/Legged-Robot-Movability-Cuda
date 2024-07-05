493b605c3b45bcada5f02fe0dc6a2c5bd5d3bb17
Elian NEPPEL <70491689+hubble14567@users.noreply.github.com>
2024-07-05 18:52:01 +0900

    beautifuller graph

--- benchIllu.py
+++ benchIllu.py
@@ -43,7 +43,7 @@ file_list_r = [
     },
     {
         D: "./bdata/pc/rbdl.csv",
-        N: "rbdl",
+        N: "pcrbdl",
     },
     {
         D: "./bdata/jetson/rgpu.csv",
@@ -55,7 +55,7 @@ file_list_r = [
     },
     {
         D: "./bdata/jetson/rbdl.csv",
-        N: "rbdl",
+        N: "jetrbdl",
     },
 ]
 
@@ -81,24 +81,24 @@ file_list_d = [
 plt.rcParams.update(
     {
         "font.family": "serif",
-        "font.size": 16,
+        "font.size": 19,
         "font.serif": ["DejaVu Serif"],
         # 'axes.titlesize': 16,
-        # 'axes.labelsize': 14,
-        # 'legend.fontsize': 12,
-        # 'xtick.labelsize': 12,
-        # 'ytick.labelsize': 12,
+        # 'axes.labelsize': 17,
+        'legend.fontsize': 15,
+        "xtick.labelsize": 16,
+        "ytick.labelsize": 15,
         "lines.linewidth": 3,  # Default line width
         "lines.markersize": 12,  # Default marker size
     }
 )
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 9), sharex=True)
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10), sharex=True)
 
 for file in file_list_r:
     path = file[D]
     name = file[N]
-    if name == "rbdl":
+    if "rbdl" in name:
         continue
     # Read the CSV file using numpy
     data = np.genfromtxt(path, delimiter=";")
@@ -119,7 +119,7 @@ for file in file_list_r:
 for file in file_list_d:
     path = file[D]
     name = file[N]
-    if name == "rbdl":
+    if "rbdl" in name:
         continue
     # Read the CSV file using numpy
     data = np.genfromtxt(path, delimiter=";")
@@ -152,10 +152,10 @@ for ind, ax in enumerate([ax1, ax2]):
     ax.grid(True, which="minor", ls="-", linewidth=0.4, color="black", alpha=0.1)
     ax.spines["top"].set_visible(False)
     ax.spines["right"].set_visible(False)
+    # ax.spines['bottom'].set_visible(False)
+    ax.spines["left"].set_visible(False)
     ax.yaxis.set_major_formatter(ScalarFormatter())
     ax.yaxis.get_major_formatter().set_scientific(False)
-    # ax.spines['bottom'].set_visible(False)
-    ax.spines['left'].set_visible(False)
 
 # Extract the legend handles and labels
 handles, labels = ax1.get_legend_handles_labels()
@@ -172,7 +172,7 @@ legend_ax.legend(
     ncol=2,
     # handleheight=2.5,
     handlelength=4.0,
-    fontsize=14,
+    # fontsize=14,
     facecolor="None",  # Set background color
     edgecolor="None",  # Set border color
 )
@@ -186,3 +186,155 @@ plt.savefig(f"image/benchIllu.jpg", bbox_inches="tight", dpi=150)
 plt.savefig(f"image/benchIllu.png", bbox_inches="tight", dpi=150)
 plt.savefig(f"image/benchIllu.pdf", bbox_inches="tight", dpi=150)
 plt.savefig(f"image/benchIllu.eps", bbox_inches="tight", dpi=150)
+
+# Set global font properties
+plt.rcParams.update(
+    {
+        "font.family": "serif",
+        "font.size": 18,
+        "font.serif": ["DejaVu Serif"],
+        # 'axes.titlesize': 16,
+        # 'axes.labelsize': 17,
+        # 'legend.fontsize': 12,
+        "xtick.labelsize": 18,
+        "ytick.labelsize": 14,
+        "lines.linewidth": 3,  # Default line width
+        "lines.markersize": 12,  # Default marker size
+    }
+)
+
+# Sample data
+# categories = [PC_GPU, PC_CPU, JET_GPU, JET_CPU]
+categories = ["GPU\n1080Ti", "CPU\ni5-12600K",  "GPU Jetson\nOrin NX",   "GPU Jetson\nOrin NX"]
+subcategories = ["Proposed\nReachability", "Proposed\nDistance", "Levenberg-Marquardt"]
+values = np.array(
+    [
+        [4, 7, np.nan],
+        [1, 8, np.nan],
+        [5, 3, np.nan],
+        [1, 2, np.nan],
+    ]
+)  # Values for each group and subcategory
+
+for file in file_list_r:
+    path = file[D]
+    name = file[N]
+    col = 0
+    row = 0
+    if name == PC_GPU:
+        row = 0
+    if name == PC_CPU:
+        row = 1
+    if name == JET_GPU:
+        row = 2
+    if name == JET_CPU:
+        row = 3
+    if name == "pcrbdl":
+        row = 1
+        col = 2
+    if name == "jetrbdl":
+        row = 3
+        col = 2
+    # Read the CSV file using numpy
+    data = np.genfromtxt(path, delimiter=";")
+    data = data[data[:, 0] > 10_000, :]
+    values[row, col] = sum(data[:, 1]) / data.shape[0]
+
+for file in file_list_d:
+    path = file[D]
+    name = file[N]
+    col = 1
+    row = 0
+    if name == PC_GPU:
+        row = 0
+    if name == PC_CPU:
+        row = 1
+    if name == JET_GPU:
+        row = 2
+    if name == JET_CPU:
+        row = 3
+    # Read the CSV file using numpy
+    data = np.genfromtxt(path, delimiter=";")
+    values[row, col] = sum(data[:, 1]) / data.shape[0]
+
+# Number of groups and subcategories
+n_groups = len(categories)
+n_subcategories = len(subcategories)
+colors = ["black", "grey", "firebrick"]
+
+# Bar width
+bar_width = 0.3
+slim = 0.9
+
+# X locations for the groups
+index = np.arange(n_groups)
+
+# Create a figure and axis
+fig, ax = plt.subplots(figsize=(10, 4))
+
+# Plot bars for each subcategory
+for i in range(n_subcategories):
+    bars = plt.bar(
+        index + i * bar_width,
+        values[:, i],
+        bar_width * slim,
+        label=subcategories[i],
+        color=colors[i],
+    )
+    # Add labels on top of each bar
+    for bar in bars:
+        yval = bar.get_height()
+        if yval >= 10:
+            roundedval = int(round(yval, 0))
+        else:
+            roundedval = round(yval, 1)
+        plt.text(
+            bar.get_x() + bar.get_width() / 2.0,
+            yval + 0.1,
+            roundedval,
+            ha="center",
+            va="bottom",
+            fontsize = 14,
+            bbox=dict(facecolor='white', edgecolor='none', pad=0),
+        )
+# Add labels and title
+plt.yscale("log")
+# plt.xlabel("Categories")
+plt.ylabel("Average time (ns/sample)")
+# plt.title("Grouped Bar Chart")
+plt.xticks(index + bar_width / 2 * (n_subcategories - 1), categories)
+# plt.legend()
+
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+# ax.spines['bottom'].set_visible(False)
+ax.spines["left"].set_visible(False)
+ax.set_axisbelow(True)
+plt.grid(axis="y", linewidth=2, color="black", alpha=0.2)
+
+# Extract the legend handles and labels
+handles, labels = ax.get_legend_handles_labels()
+
+# Create an axis for the legend
+legend_ax = fig.add_axes([0.07, 0.98, 0.8, 0.04], frame_on=False)
+legend_ax.axis("off")  # Hide the axis
+
+# Add the legend to the custom axis
+legend_ax.legend(
+    handles=handles,
+    labels=labels,
+    loc="center",
+    ncol=3,
+    handleheight=1,
+    # handlelength=4.0,
+    # fontsize=15,
+    facecolor="None",  # Set background color
+    edgecolor="None",  # Set border color
+)
+
+plt.subplots_adjust(top=0.90, hspace=0.02)
+
+plt.savefig(f"image/benchBar.jpg", bbox_inches="tight", dpi=150)
+plt.savefig(f"image/benchBar.png", bbox_inches="tight", dpi=150)
+plt.savefig(f"image/benchBar.pdf", bbox_inches="tight", dpi=150)
+plt.savefig(f"image/benchBar.eps", bbox_inches="tight", dpi=150)
